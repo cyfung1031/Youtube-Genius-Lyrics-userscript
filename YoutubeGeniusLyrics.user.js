94c87261ac72ba00e6d931a5e3de9545e0a44467
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2022-12-20 16:45:03 +0900

    Update YoutubeGeniusLyrics.user.js

--- YoutubeGeniusLyrics.user.js
+++ YoutubeGeniusLyrics.user.js
@@ -14,7 +14,7 @@
 // @icon            https://raw.githubusercontent.com/hfg-gmuend/openmoji/master/color/72x72/E044.png
 // @supportURL      https://github.com/cvzi/Youtube-Genius-Lyrics-userscript/issues
 // @version         10.9.2
-// @require         https://greasyfork.org/scripts/406698-geniuslyrics/code/GeniusLyrics.js
+// @require         https://greasyfork.org/scripts/406698-geniuslyrics/code/GeniusLyrics.js?
 // @grant           GM.xmlHttpRequest
 // @grant           GM.setValue
 // @grant           GM.getValue
